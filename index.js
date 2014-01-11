65ece73e49de01db77ec064c21f1fc85741aa6d7
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-11 06:53:35 -0600

    Fixed bootstrap config.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+var util = require('util');
 var _ = require('lodash');
+var path = require('path');
+
 
 
 /**
@@ -12,10 +15,25 @@ var _ = require('lodash');
 module.exports = {
 
 	bootstrap: function (scope, cb) {
+
+		if ( !_.isArray(scope.args) ) {
+			return cb(new Error('Invalid `scope.args` passed to generator: '+util.inspect(scope.args)));
+		}
+
+		// If no arg specified, use 'newGenerator' as the name:
+		if ( !scope.args[0] || !_.isString(scope.args[0])) {
+			scope.args[0] = 'newGenerator';
+		}
+
 		_.defaults(scope, {
 			moduleName: 'sails-generate-'+scope.args[0],
 			author: 'A Node.js/Sails.js Contributor',
 			year: (new Date()).getFullYear()
+		});
+
+		// Where to create the generator
+		_.merge(scope, {
+			destPath: path.join(scope.destPath, scope.args[0])
 		});
 		cb();
 	},
@@ -23,7 +41,7 @@ module.exports = {
 	targets: {
 		'.': {
 			exec: function (scope, cb) {
-				console.log('TODO: generate stuff starting @ `'+scope.relPath+'`...');
+				console.log('TODO: generate stuff starting @ `'+scope.destPath+'`...');
 				cb();
 			}
 		}
